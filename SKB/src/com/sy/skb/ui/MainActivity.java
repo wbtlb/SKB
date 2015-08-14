576fe6a19dbce9991aa2cca0a84ee32574a60fc6
wbtlb <875818649@qq.com>
2015-08-14 17:13:35 +0800

    加入瀑布流

--- SKB/src/com/sy/skb/ui/MainActivity.java
+++ SKB/src/com/sy/skb/ui/MainActivity.java
@@ -1,13 +1,23 @@
 package com.sy.skb.ui;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.AsyncTask.Status;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -17,7 +27,11 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import cn.bmob.v3.Bmob;
 
+import com.dodola.model.DuitangInfo;
 import com.sy.skb.R;
+import com.sy.skb.bitmapfunutil.Helper;
+import com.sy.skb.bitmapfunutil.ImageFetcher;
+import com.sy.skb.libraryviews.StaggeredGridView;
 
 public class MainActivity extends Activity {
 	private String[] names = new String[] { "香酥小蟹肉饼", "李白", "张三", "李四" };
@@ -40,6 +54,10 @@ public class MainActivity extends Activity {
 	private LinearLayout layout_title = null;
 	private ListView left_menu = null;
 
+	private ImageFetcher mImageFetcher;
+	private StaggeredAdapter mAdapter;
+	private ContentTask task = new ContentTask(this, 2);
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -61,7 +79,7 @@ public class MainActivity extends Activity {
 		cuan = (TextView) findViewById(R.id.cuan);
 		title = (TextView) findViewById(R.id.textView_title);
 		layout_title = (LinearLayout) findViewById(R.id.layout_title);
-		left_menu = (ListView) findViewById(R.id.left_menu);
+		//left_menu = (ListView) findViewById(R.id.left_menu);
 
 		find.setOnClickListener(listener);
 		chao.setOnClickListener(listener);
@@ -77,7 +95,21 @@ public class MainActivity extends Activity {
 		title.setOnClickListener(listener);
 		layout_title.setOnClickListener(listener);
 
-		
+		mImageFetcher = new ImageFetcher(this, 240);
+		mImageFetcher.setLoadingImage(R.drawable.empty_photo);
+		StaggeredGridView gridView = (StaggeredGridView) this
+				.findViewById(R.id.staggeredGridView1);
+
+		int margin = getResources().getDimensionPixelSize(R.dimen.margin);
+
+		gridView.setFastScrollEnabled(true);
+
+		mAdapter = new StaggeredAdapter(MainActivity.this, mImageFetcher);
+		gridView.setAdapter(mAdapter);
+		mAdapter.notifyDataSetChanged();
+		AddItemToContainer(1, 1);
+		AddItemToContainer(2, 1);
+		AddItemToContainer(3, 1);
 		// final BmobQuery<Menu> query = new BmobQuery<Menu>();
 		// query.getObject(this,"5GtL999R",new GetListener<Menu>(){
 		//
@@ -111,8 +143,8 @@ public class MainActivity extends Activity {
 				R.layout.item, new String[] { "personName", "header", "desc",
 						"personName", "header", "desc" }, new int[] {
 						R.id.name, R.id.header, R.id.desc });
-		ListView list = (ListView) findViewById(R.id.left_menu);
-		list.setAdapter(simpleAdapter);
+		//ListView list = (ListView) findViewById(R.id.left_menu);
+		//list.setAdapter(simpleAdapter);
 	}
 
 	public OnClickListener listener = new OnClickListener() {
@@ -176,4 +208,106 @@ public class MainActivity extends Activity {
 		}
 
 	};
+
+	private void AddItemToContainer(int pageindex, int type) {
+		if (task.getStatus() != Status.RUNNING) {
+			String url = "http://www.duitang.com/album/1733789/masn/p/"
+					+ pageindex + "/24/";
+			Log.d("MainActivity", "current url:" + url);
+			ContentTask task = new ContentTask(this, type);
+			task.execute(url);
+
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private class ContentTask extends
+			AsyncTask<String, Integer, List<DuitangInfo>> {
+
+		private Context mContext;
+		private int mType = 1;
+
+		public ContentTask(Context context, int type) {
+			super();
+			mContext = context;
+			mType = type;
+		}
+
+		@Override
+		protected List<DuitangInfo> doInBackground(String... params) {
+			try {
+				return parseNewsJSON(params[0]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(List<DuitangInfo> result) {
+			if (mType == 1) {
+
+				mAdapter.addItemTop(result);
+				mAdapter.notifyDataSetChanged();
+
+			} else if (mType == 2) {
+				mAdapter.addItemLast(result);
+				mAdapter.notifyDataSetChanged();
+
+			}
+
+		}
+
+		@Override
+		protected void onPreExecute() {
+		}
+
+		public List<DuitangInfo> parseNewsJSON(String url) throws IOException {
+			List<DuitangInfo> duitangs = new ArrayList<DuitangInfo>();
+			String json = "";
+			if (Helper.checkConnection(mContext)) {
+				try {
+					json = Helper.getStringFromUrl(url);
+
+				} catch (IOException e) {
+					Log.e("IOException is : ", e.toString());
+					e.printStackTrace();
+					return duitangs;
+				}
+			}
+			Log.d("MainActiivty", "json:" + json);
+
+			try {
+				if (null != json) {
+					JSONObject newsObject = new JSONObject(json);
+					JSONObject jsonObject = newsObject.getJSONObject("data");
+					JSONArray blogsJson = jsonObject.getJSONArray("blogs");
+
+					for (int i = 0; i < blogsJson.length(); i++) {
+						JSONObject newsInfoLeftObject = blogsJson
+								.getJSONObject(i);
+						DuitangInfo newsInfo1 = new DuitangInfo();
+						newsInfo1
+								.setAlbid(newsInfoLeftObject.isNull("albid") ? ""
+										: newsInfoLeftObject.getString("albid"));
+						newsInfo1
+								.setIsrc(newsInfoLeftObject.isNull("isrc") ? ""
+										: newsInfoLeftObject.getString("isrc"));
+						newsInfo1.setMsg(newsInfoLeftObject.isNull("msg") ? ""
+								: newsInfoLeftObject.getString("msg"));
+						newsInfo1.setHeight(newsInfoLeftObject.getInt("iht"));
+						duitangs.add(newsInfo1);
+					}
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			return duitangs;
+		}
+	}
 }
